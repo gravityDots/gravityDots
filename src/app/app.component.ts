bfa35464729a863af414953388043a5851baff7e
mkateliya <mkateliya@asite.com>
2024-06-09 15:28:12 +0530

    chnage other services png size and add loader for body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent{
+export class AppComponent implements OnInit{
+  isLoading = true;
 
+  ngOnInit(): void {
+    this.loadContent();
+  }
+
+  loadContent(): void {
+    window.addEventListener('load', () => {
+      this.isLoading = false;
+    });
+  }
 }
